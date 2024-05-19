0c24053762605f3f0ceb84e4e8bb0b7ea97e07ba
Keval Patel <kvpatel1682001@gmail.com>
2024-05-19 03:34:07 -0400

    calculator

---
+++ CalculatorUI/CalculatorUI/ViewController.swift
@@ -0,0 +1,101 @@
+//
+//  ViewController.swift
+//  CalculatorUI
+//
+//  Created by user244717 on 5/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func buttonClear(_ sender: UIButton) {
+        print("AC")
+    }
+    @IBAction func buttonDot(_ sender: UIButton) {
+        print(".")
+    }
+    @IBAction func buttonEqual(_ sender: UIButton) {
+        print("=")
+    }
+    @IBAction func buttonAdd(_ sender: UIButton) {
+        print("+")
+    }
+    @IBAction func buttonSubstract(_ sender: UIButton) {
+        print("-")
+    }
+    @IBAction func buttonMultiply(_ sender: UIButton) {
+        print("x")
+    }
+    @IBAction func buttonDivide(_ sender: UIButton) {
+        print("/")
+    }
+    @IBAction func buttonSquare(_ sender: UIButton) {
+        print("^2")
+    }
+    @IBAction func buttonInvert(_ sender: UIButton) {
+        print("+/-")
+    }
+    
+    @IBAction func buttonZero(_ sender: UIButton) {
+        print("0")
+    }
+    @IBAction func buttonOne(_ sender: UIButton) {
+        print("1")
+    }
+    @IBAction func buttonTwo(_ sender: UIButton) {
+        print("2")
+    }
+    @IBAction func buttonThree(_ sender: UIButton) {
+        print("3")
+    }
+    @IBAction func buttonFour(_ sender: UIButton) {
+        print("4")
+    }
+    @IBAction func buttonFive(_ sender: UIButton) {
+        print("5")
+    }
+    @IBAction func buttonSix(_ sender: UIButton) {
+        print("6")
+    }
+    @IBAction func buttonSeven(_ sender: UIButton) {
+        print("7")
+    }
+    @IBAction func buttonEight(_ sender: UIButton) {
+        print("8")
+    }
+    @IBAction func buttonNine(_ sender: UIButton) {
+        print("9")
+    }
+    
+    @IBAction func DigitOne(_ sender: UITextField) {
+    }
+    @IBAction func DigitTwo(_ sender: UITextField) {
+    }
+    @IBOutlet weak var Result: UILabel!
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
+
