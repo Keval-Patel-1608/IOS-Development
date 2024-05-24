063a48455ed359974e1db8666e7ff6010c272a7a
Keval Patel <kvpatel1682001@gmail.com>
2024-05-23 20:44:15 -0400

    User Input Form

---
+++ Week 3/UserInputForm/UserInputForm/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  UserInputForm
+//
+//  Created by user244717 on 5/23/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBOutlet weak var txtFirstName: UITextField!
+    @IBOutlet weak var txtLastName: UITextField!
+    @IBOutlet weak var txtCountry: UITextField!
+    @IBOutlet weak var txtAge: UITextField!
+    @IBOutlet weak var txtArea: UITextView!
+    @IBOutlet weak var labelMessage: UILabel!
+    
+    @IBAction func buttonAdd(_ sender: UIButton) {
+        self.txtArea.text = "Full Name : \(self.txtFirstName.text!) \(self.txtLastName.text!)\nCountry : \(self.txtCountry.text!)\nAge : \(self.txtAge.text!)"
+    }
+    @IBAction func buttonSubmit(_ sender: UIButton) {
+        if self.txtFirstName.text == "" && self.txtLastName.text == "" && self.txtCountry.text == "" && self.txtAge.text == ""{
+            self.labelMessage.text = "Complete The Missing Info!"
+        }else {
+            self.labelMessage.text = "Successfully Submitted"
+        }
+            
+    }
+    @IBAction func buttonClear(_ sender: UIButton) {
+        self.txtFirstName.text = ""
+        self.txtLastName.text = ""
+        self.txtCountry.text = ""
+        self.txtAge.text = ""
+        self.txtArea.text = ""
+        self.labelMessage.text = ""
+    }
+    
+}
+
