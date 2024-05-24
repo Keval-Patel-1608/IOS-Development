f8cac3d7aca14cafe0a166e0397e6f5e2efaa83e
Keval Patel <kvpatel1682001@gmail.com>
2024-05-24 03:55:24 -0400

    Update ViewController.swift

--- Week 3/UserInputForm/UserInputForm/ViewController.swift
+++ Week 3/UserInputForm/UserInputForm/ViewController.swift
@@ -11,7 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     @IBOutlet weak var txtFirstName: UITextField!
