57e6b3306fc4adf600a3caca595d53e0ce5b2dac
Keval Patel <kvpatel1682001@gmail.com>
2024-06-05 14:51:27 -0400

    lab 5

---
+++ week5_ex/week5_ex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  week5_ex
+//
+//  Created by user244717 on 6/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
