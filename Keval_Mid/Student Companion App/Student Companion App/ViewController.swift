79d26308db3d118995544a7a9fd99b6486d2657f
Keval Patel <kvpatel1682001@gmail.com>
2024-07-09 20:38:44 -0400

    mid update

---
+++ Keval_Mid/Student Companion App/Student Companion App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Student Companion App
+//
+//  Created by user244717 on 7/9/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
