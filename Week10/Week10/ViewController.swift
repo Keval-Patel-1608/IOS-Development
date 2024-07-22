73093997985b44fde5855266c7b8cf10cf713e7a
Keval Patel <kvpatel1682001@gmail.com>
2024-07-22 17:07:41 -0400

    weather app

--- Week10/Week10/ViewController.swift
+++ Week10/Week10/ViewController.swift
@@ -1,19 +1,98 @@
-//
-//  ViewController.swift
-//  Week10
-//
-//  Created by user244717 on 7/15/24.
-//
-
 import UIKit
+import CoreLocation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var locationDisplay: UILabel!
+    @IBOutlet weak var weatherDisplay: UILabel!
+    @IBOutlet weak var weatherIconDisplay: UIImageView!
+    @IBOutlet weak var temperatureDisplay: UILabel!
+    @IBOutlet weak var humidityDisplay: UILabel!
+    @IBOutlet weak var windDisplay: UILabel!
+
+    let locationManager = CLLocationManager()
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           
+           locationManager.delegate = self
+           locationManager.requestWhenInUseAuthorization()
+           locationManager.startUpdatingLocation()
+           
+           // Additional logging
+           print("viewDidLoad: Location Manager started.")
+       }
+       
+       func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+           if let location = locations.last {
+               let lat = location.coordinate.latitude
+               let lon = location.coordinate.longitude
+               print("Location updated: lat: \(lat), lon: \(lon)")
+               fetchWeather(latitude: lat, longitude: lon)
+           }
+       }
+
+    // MARK: - Networking
+    func fetchWeather(latitude: Double, longitude: Double) {
+        let apiKey = "0396aac6351c1459c974171411e1a72e"
+        let urlString = "https://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)&units=metric"
+
+        guard let url = URL(string: urlString) else { return }
+
+        let urlSession = URLSession(configuration: .default)
+        let dataTask = urlSession.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Error fetching weather data: \(error)")
+                return
+            }
+
+            guard let data = data else { return }
+
+            do {
+                let jsonDecoder = JSONDecoder()
+                let weather = try jsonDecoder.decode(Weather.self, from: data)
+                DispatchQueue.main.async {
+                    self.updateUI(with: weather)
+                }
+            } catch {
+                print("Failed to decode weather data: \(error)")
+            }
+        }
+        dataTask.resume()
     }
 
+    // MARK: - UI Update
+    func updateUI(with weather: Weather) {
+        locationDisplay.text = weather.name
+        weatherDisplay.text = weather.weather.first?.description
+        temperatureDisplay.text = "\(Int(weather.main.temp))°C"
+        humidityDisplay.text = "\(weather.main.humidity)%"
+        
+        let windSpeedKmH = Int(weather.wind.speed * 3.6)
+        windDisplay.text = "\(windSpeedKmH) km/h"
+        
+        if let icon = weather.weather.first?.icon {
+            fetchWeatherIcon(for: icon)
+        }
+    }
 
+    func fetchWeatherIcon(for icon: String) {
+        let urlString = "https://openweathermap.org/img/wn/\(icon)@2x.png"
+        guard let url = URL(string: urlString) else { return }
+
+        let urlSession = URLSession(configuration: .default)
+        let dataTask = urlSession.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print("Error fetching weather icon: \(error)")
+                return
+            }
+
+            guard let data = data else { return }
+
+            DispatchQueue.main.async {
+                self.weatherIconDisplay.image = UIImage(data: data)
+            }
+        }
+        dataTask.resume()
+    }
 }
-
--- Week10/Week10/Weather.swift
+++ Week10/Week10/Weather.swift
@@ -1,13 +1,82 @@
+//import Foundation
+//
+//// MARK: - Weather
+//struct Weather: Codable {
+//    let coord: Coord
+//    let weather: [WeatherElement]
+//    let base: String
+//    let main: Main
+//    let visibility: Int
+//    let wind: Wind
+//    let clouds: Clouds
+//    let dt: Int
+//    let sys: Sys
+//    let timezone, id: Int
+//    let name: String
+//    let cod: Int
+//}
+//
+//// MARK: - Clouds
+//struct Clouds: Codable {
+//    let all: Int
+//}
+//
+//// MARK: - Coord
+//struct Coord: Codable {
+//    let lon, lat: Double
+//}
+//
+//// MARK: - Main
+//struct Main: Codable {
+//    let temp, feelsLike, tempMin, tempMax: Double
+//    let pressure, humidity, seaLevel, grndLevel: Int
+//
+//    enum CodingKeys: String, CodingKey {
+//        case temp
+//        case feelsLike = "feels_like"
+//        case tempMin = "temp_min"
+//        case tempMax = "temp_max"
+//        case pressure, humidity
+//        case seaLevel = "sea_level"
+//        case grndLevel = "grnd_level"
+//    }
+//}
+//
+//// MARK: - Sys
+//struct Sys: Codable {
+//    let type, id: Int
+//    let country: String
+//    let sunrise, sunset: Int
+//}
+//
+//// MARK: - WeatherElement
+//struct WeatherElement: Codable {
+//    let id: Int
+//    let main, description, icon: String
+//}
+//
+//// MARK: - Wind
+//struct Wind: Codable {
+//    let speed: Double
+//    let deg: Int
+//}
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let weather = try? JSONDecoder().decode(Weather.self, from: jsonData)
+
 import Foundation
 
 // MARK: - Weather
-struct Weather: Codable {
+struct Weather:Codable {
     let coord: Coord
     let weather: [WeatherElement]
     let base: String
     let main: Main
     let visibility: Int
     let wind: Wind
+    let rain: Rain?
     let clouds: Clouds
     let dt: Int
     let sys: Sys
@@ -17,77 +86,43 @@ struct Weather: Codable {
 }
 
 // MARK: - Clouds
-struct Clouds: Codable {
+struct Clouds:Codable {
     let all: Int
 }
 
 // MARK: - Coord
-struct Coord: Codable {
+struct Coord:Codable {
     let lon, lat: Double
 }
 
 // MARK: - Main
 struct Main: Codable {
-    let temp, feelsLike, tempMin, tempMax: Double
-    let pressure, humidity, seaLevel, grndLevel: Int
+    let temp: Double
+    let tempMin, tempMax, feelsLike: Double?
+    let pressure, seaLevel, grndLevel: Int?
+    let humidity: Int
+}
 
-    enum CodingKeys: String, CodingKey {
-        case temp
-        case feelsLike = "feels_like"
-        case tempMin = "temp_min"
-        case tempMax = "temp_max"
-        case pressure, humidity
-        case seaLevel = "sea_level"
-        case grndLevel = "grnd_level"
-    }
+// MARK: - Rain
+struct Rain:Codable {
+    let the1H: Int
 }
 
 // MARK: - Sys
-struct Sys: Codable {
-    let type, id: Int
+struct Sys:Codable {
     let country: String
     let sunrise, sunset: Int
 }
 
 // MARK: - WeatherElement
-struct WeatherElement: Codable {
+struct WeatherElement:Codable {
     let id: Int
     let main, description, icon: String
 }
 
 // MARK: - Wind
-struct Wind: Codable {
+struct Wind:Codable {
     let speed: Double
     let deg: Int
     let gust: Double
-}
-
-//API Session
-    let urlString = "https://api.openweathermap.org/data/2.5/weather?q=Waterloo,CA&appid=0396aac6351c1459c974171411e1a72e"
-
-    let urlSession = URLSession(configuration: .default)
-    let url = URL(string: urlString)
-
-if let url = url {
-    let dataTask = urlSession.dataTask(with: url) { (data, response, error) in
-        
-        if let data = data {
-            print(data)
-            let jsonDecoder = JSONDecoder()
-            do {
-                let readableData = try jsonDecoder.decode(Weather.self, from: data)
-                print (readableData)
-                print (readableData)
-                print (readableData)
-                print (readableData)
-                print (readableData)
-                print (readableData)
-            }
-            catch {
-                print ("Can't Decode Data")
-            }
-        }
-    }
-    dataTask.resume()
-    dataTask.response
 }
